abe8d5654b35e91b53427396d483cf5904f744f4
Asadbek <asadbekrakhmonov.00@gmail.com>
2023-07-25 21:14:23 +0500

    now

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,33 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.scss'
 import all from '../../images/all.svg'
 import categories from '../../data/categories.json'
-import data from '../../data/products.json'
 
 const Header = () => {
-    const [activeSortCategory, setactiveSortCategory] = useState(0);
-    // const [activeSortPrice, setactiveSortPrice] = useState(50);
-    // const [activeSortSize, setactiveSortSize] = useState(0);
-    // const [sortProductCategory, setsortProductCategory] = useState(1);
-    // // const [products, setproducts] = useState(data);
-    // const [sortedCategory, setsortedCategory] = useState(false);
-    // const [activePagination, setactivePagination] = useState(0);
-    // // const dispatch = useDispatch();
-    // const { cart } = useSelector(state => state);
+    const [categoriesdata, setCategoriesdata] = useState([]);
+
+  useEffect(() => {
+    setCategoriesdata(categories);
+  }, []);
+
+  // const activeCategory = {
+  //   "fontWeight": "700",
+  //   "color": "#46A358"
+  // };
   
-    // // const dispatchProduct = (data) => {
-    // //   const action = {
-    // //     type: "ADD_TO_INITIALCART",
-    // //     data: data
-    // //   }
-  
-    // //   dispatch(action);
-    // // }
-    
-  const activeCategory = {
-    "fontWeight": "700",
-    "color": "#46A358"
-  };
 
   return (
     <div className='header'>
@@ -37,23 +24,13 @@ const Header = () => {
                 <h1 className='header_pages_collection_all'>
                     All Collections
                 </h1>
-                <p className='header_pages_collection_p'>
-                    {
-                      categories.map(({ id, name }) => {
-                        return <li key={id} className='header_pages_collection_p_item'
-                          onClick={(() => setactiveSortCategory((e) => e === id ? 0 : id))}
-                        >
-                          <span style={activeSortCategory === id ? activeCategory : null}>{name}</span>
-                          <span style={activeSortCategory === id ? activeCategory : null}>({
-                            data.filter(({ categoryId }) => {
-                              return categoryId === id
-                            }).length
-                          })</span>
-                        </li>
-                      })
-                    }
-                </p>
-
+                {/* <p className='header_pages_collection_p'>
+                    {categoriesdata.map((category) => (
+                      <span key={category.id} className="header_pages_collection_p_item">
+                        {category.name}
+                      </span>
+                    ))}
+                </p> */}
             </div>
             <div className='header_pages_page'>
                 <div className='header_pages_page_title'>
