da3bde5184e3cb649c289ed62dfc8cb56a6eddc3
Asadbek <asadbekrakhmonov.00@gmail.com>
2023-08-04 07:46:27 +0500

    23

--- src/Component/Content/Content.js
+++ src/Component/Content/Content.js
@@ -3,6 +3,9 @@ import shipping from '../../images/shipping.svg'
 import gift from '../../images/gift.svg'
 import quick from '../../images/quick.svg'
 import support from '../../images/support.svg'
+import modern from '../../images/modern.png'
+import bookcase from '../../images/bookcase.png'
+import sofa from '../../images/sofa.png'
 import './Content.scss'
 
 
@@ -15,23 +18,25 @@ const Content = () => {
           <div className='content_cards_shipping'>
             <img src={shipping} className='content_cards_shipping_img' />
             <p className='content_cards_shipping_p'>
-              Free Shipping
-            </p>
-             <span className='content_cards_shipping_span'>
-              Orders over $100
+                Free Shipping
+              <span className='content_cards_shipping_p_span'>
+                Orders over $100
              </span>
+            </p>
+            
           </div>
-        </div>
+        
 
         {/* Gift  */}
         <div className='content_cards_gift'>
           <img src={gift} className='content_cards_gift_img' />
           <p className='content_cards_gift_p' >
             Smart Gift Card
+            <span className='content_cards_gift_p_span'>
+               Buy $1000 to get card
+            </span>
           </p>
-          <span className='content_cards_gift_span'>
-            Buy 1000
-          </span>
+          
         </div>
 
         {/* Quick  */}
@@ -39,23 +44,70 @@ const Content = () => {
           <img src={quick} className='content_cards_quick_img' />
           <p className='content_cards_quick_p' >
             Quick Payment
+              <span className='content_cards_quick_p_span'>
+                100% secure payment
+              </span>  
           </p>
-          <span className='content_cards_quick_span'>
-            Buy 1000
-          </span>
         </div>
 
         {/* Support  */}
         <div className='content_cards_support'>
           <img src={support} className='content_cards_support_img' />
           <p className='content_cards_support_p' >
-            Quick Payment
+                24/7 Support
+              <span className='content_cards_support_p_span'>
+                Quick support
+              </span>
           </p>
-          <span className='content_cards_support_span'>
-            Buy 1000
-          </span>
+          
         </div>
+      </div>  
+          {/* Modern  */}
+      <div className='content_discont'>
         
+        <div className='content_discont_modern'>
+          <img src={modern} className='content_discont_modern_img' />
+          <h2 className='content_discont_modern_h2'>
+            Modern Furniture Collections
+          </h2>
+          <span className='content_discont_modern_span'>
+            Starting from $500
+          </span>
+          <p className='content_discont_modern_p'>
+            Read more
+          </p>
+
+        </div>
+            {/* Bookcase  */}
+        <div className='content_discont_bookcase'>
+            
+            <h2 className='content_discont_bookcase_h2'>
+              Geometric Bookcase
+            </h2>
+            <span className='content_discont_bookcase_span'>
+              Up to 20% discount
+            </span>
+            <button className='content_discont_bookcase_button'>
+              Read mode
+            </button>
+            <img src={bookcase} className='content_discont_bookcase_img' />
+        </div>
+
+        {/* Sofa */}
+        <div className='content_discont_sofa'>
+          <img src={sofa} className='content_discont_sofa_img' />
+          {/* <h2 className='content_discont_sofa_h2'>
+            Minimal Sofa collections 
+          </h2>
+          <span className='content_discont_sofa_span'>
+            Sale upto 40% discount
+          </span>
+          <button className='content_discont_sofa_button'>
+            Read mode
+          </button> */}
+
+        </div>
+      </div>
     </div>
   )
 }
